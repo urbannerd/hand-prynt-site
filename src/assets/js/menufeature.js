cd66db973fecbb24b711f54a8c7593e14ba7a960
cooleycel <marcelstefon@gmail.com>
2019-09-23 01:19:00 -0400

    video player completed and updated menu

--- src/assets/js/menufeature.js
+++ src/assets/js/menufeature.js
@@ -2,10 +2,18 @@ $(document).ready(function () {
   $('#toggle').click(function() {
     $(this).toggleClass('active');
     $('#overlay').toggleClass('open');
+    
    });
+
    $('#overlay li').on('click', function(){
     $("#overlay").hide();
     $("#toggle").removeClass("active");
+});
+
+$('.close-overlay').click(function() {
+  $("#overlay").hide();
+  $('#toggle').removeClass('active');
+
 });
 }); // End of use strict
 
