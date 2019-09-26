4982a857555b0d2afeaf36f11c4fe1a1ff00a701
marcelmcduffie <marcelmcduffie@appliedis.com>
2019-09-25 22:35:38 -0400

    dist folder 10:35 9-25

--- dist/assets/js/menufeature.js
+++ dist/assets/js/menufeature.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
   $('#toggle-mobile').click(function() {
-    $('#overlay').toggleClass('open');  
+    $('#overlay').toggleClass('open'); 
    });
   
   $('#toggle').click(function() {
@@ -8,11 +8,9 @@ $(document).ready(function () {
    });
 
    $('#overlay li').on('click', function(){
-    $("#overlay").hide();
-    $("#toggle").removeClass("active");
+    $(".open").removeClass('open');
+    setTimeout(function(){window.location = window.location}, 3);
+    
 });
-
-
-
 }); // End of use strict
 
