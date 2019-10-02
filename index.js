db2ec0e720e7bbe2e6599a7472ba85efb3dc7eb9
cooleycel <marcelstefon@gmail.com>
2019-10-02 12:13:22 -0400

    contact functions changed

--- index.js
+++
@@ -1,7 +0,0 @@
-const http = require('http');
-const app = require('./app');
-require('dotenv').config({path: './vars.env'});
-const server = http.createServer(app);
-
-const port = 3000;
-server.listen(process.env.PORT_NUMBER)
