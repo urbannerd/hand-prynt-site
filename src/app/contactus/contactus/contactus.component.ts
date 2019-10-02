c2f57b0a3169023e0437a1b2fe411df71d5db7fb
cooleycel <marcelstefon@gmail.com>
2019-10-02 10:54:18 -0400

    contact function test removed

--- src/app/contactus/contactus/contactus.component.ts
+++ src/app/contactus/contactus/contactus.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { SubmitterService } from 'src/app/submitter.service';
-import { NgForm } from '@angular/forms';
+
 
 @Component({
   selector: 'app-contactus',
@@ -9,41 +9,11 @@ import { NgForm } from '@angular/forms';
 })
 export class ContactusComponent implements OnInit {
 
-  constructor(private _service: SubmitterService) { }
+  constructor() { }
 
   ngOnInit() {
   }
 
-  contactUsForm = {
-    name: '',
-    email: '',
-    subject: '',
-    message: ''
-  };
 
-  onSubmit(testForm:NgForm){
-    this._service.postData(this.contactUsForm).subscribe(
-      data => {
-        testForm.form.reset();
-        this.contactUsForm.email='';
-        this.contactUsForm.message='';
-        this.contactUsForm.name='';
-        this.contactUsForm.subject='';
-        
-
-        let alert = document.querySelector('.alert').classList.add('alertShowed');
-        console.log(data);
-        
-        setTimeout(() => {
-          let alert = document.querySelector('.alert').classList.remove('alertShowed');
-          window.location.href = '';
-        }, 3000);
-      },
-      error => {
-        alert('Error happened');
-        console.log(error);
-      }
-      );
-    }
 
 }
